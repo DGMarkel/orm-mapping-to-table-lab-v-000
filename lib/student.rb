b4f802fde3b1363d951d102268f90ad3b323ce1d
David Markel <davidgmarkel@gmail.com>
2018-04-27 18:44:55 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,7 +35,7 @@ class Student
 
     DB[:conn].execute(sql, self.name, self.grade)
 
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")
   end
 
   def create(attributes_hash)
