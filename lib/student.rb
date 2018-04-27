52885f55b8ec7b004f45096666d9e8fbccf51b13
David Markel <davidgmarkel@gmail.com>
2018-04-27 18:20:41 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,13 @@
 class Student
   attr_accessor :name, :grade
+  attr_reader :id
 
-  def initialize(name, grade)
+  def initialize(name, grade, id = nil)
     @name = name
     @grade = grade
   end
+
+  def create_table
+    sql =
 
 end
